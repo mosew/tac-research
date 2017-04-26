c67645e2abe036b12c283477328aedf7c7cd5fcb
mosew <mose.wintner@gmail.com>
2017-04-26 10:05:11 -0700

    Some features like parameter distributions added

--- myresults.m
+++ myresults.m
@@ -4,8 +4,9 @@ names = {'26splhr_1training_M01_alltesteps','2splhr_2training_alltesteps','2splh
 
 tr_data = zeros(7,10);
 te_data = zeros(7,10);
-q1 = zeros(7,4);
-q2 = zeros(7,4);
+q = zeros(7,4);
+outliers = [];
+colinds=[];
 
 iii=0;
 for w = 1:(length(names)+3)
@@ -98,5 +99,42 @@ for w = 1:(length(names)+3)
 
     tr_data(w,:) = [training_AUC_sq,training_peakheight_sq,training_peaktime_sq,training_L2,training_Linf];
     te_data(w,:) = [test_AUC_sq,test_peakheight_sq,test_peaktime_sq,test_L2,test_Linf];
+    q(w,:)=[overall_q1,overall_q2];
+    
+% %   Distributions of the parameter estimates (M=1 omitted)
+%     subplot(2,7,w)
+%     histogram(q1);
+%     subplot(2,7,w+7)
+%     histogram(q2);
+%     
+%   Find outliers
+    measures = {'q1','q2','L2_error','Linf_error','AUC_error_sq','peak_time_error_sq','peak_height_error_sq'};
+    for j = 1:length(measures)
+        vv=eval(measures{j});
+        col = reshape(vv,numel(vv),1);
+        colinds=[];
+        for i = 1:numel(vv)
+            if mahal(col(i),col)>20
+                colinds=[colinds;i];
+                [z,x]=ind2sub(size(vv),i);
+                outliers=[outliers;w,z,x];
+%                 measures{j};
+%                 mahal(col(i),col)
+%                 vv(z,x)
+            end
+        end
+    end
+    
+%     What went wrong with test episode 4?
+    for j = 1:length(colinds)
+        plot(b{colinds(j)}.full_deconvolved_BrAC,'color',[.8,.8,.8]);
+        hold on
+    end
+    plot(u_total(4,:),'ko');
+    xlim([0,276])
+
+%    %   Looks like it just frequently overshot the uptake; could have been caused by a single bad measurement.
+
 end
-save('my_results_noteston4.mat');
+
+save('my_results.mat');

98066d0b9076abef2727ac884189ee81aec0be24
mosew <mose.wintner@gmail.com>
2017-05-29 21:16:13 -0700

    I think it works? Not totally sure, didn't want to do the Laplace version

--- pb_py/done/PB.py
+++ pb_py/done/PB.py
@@ -1,17 +1,13 @@
-
-
-
-
 class PB(object):
 
     def __init__(self):
 
-        self.P = 20
+        self.P = 5
         self.T = 1
         self.n = 50
         self.tau = 1. / 50.
-        self.burnin = 400
-        self.K = 8000
+        self.burnin = 1
+        self.K = 10
         self.alph = 3333
         self.nTheta = 2
 
@@ -19,61 +15,50 @@ class PB(object):
         self.k = 1
 
         import numpy as np
-        self.infoTheta = np.array([ [0,0], [0,0.25] ])
+        self.infoTheta = np.array([[0,0], [0,0.25]])
         self.thetas = np.zeros((self.nTheta, self.K))
-        self.thetas[:, 0] = [1, 10]
+        self.thetas[:, 0] = [1., 10.]
         self.t = np.linspace(self.tau, self.T, self.n)
         self.a = np.zeros((self.P, self.K - self.burnin))
 
         # Important matrices to be calculated
-        self.vhat = np.zeros((self.nTheta,self.nTheta))
+        self.vhat = np.zeros((self.nTheta, self.nTheta))
         self.lmatrix = np.zeros((self.P, self.n))
-        self.dlmatrix = np.zeros((self.P,self.n,self.nTheta))
-        self.d2lmatrix = np.zeros((self.P,self.n,self.nTheta,self.nTheta))
-        self.vy_th = np.zeros((self.n,self.n))
+        self.dlmatrix = np.zeros((self.P, self.n, self.nTheta))
+        self.d2lmatrix = np.zeros((self.P, self.n, self.nTheta, self.nTheta))
+        self.vy_th = np.zeros((self.n, self.n))
         self.dvy_th = np.zeros((self.n, self.n, self.nTheta))
-        self.d2vy_th = np.zeros((self.n,self.n,self.nTheta,self.nTheta))
-        self.vy_thu = np.zeros((self.n,self.n))
-        self.dvy_thu = np.zeros((self.n,self.n,self.nTheta))
-        self.d2vy_thu = np.zeros((self.n,self.n,self.nTheta,self.nTheta))
+        self.d2vy_th = np.zeros((self.n, self.n, self.nTheta, self.nTheta))
+        self.vy_thu = np.zeros((self.n, self.n))
+        self.dvy_thu = np.zeros((self.n, self.n, self.nTheta))
+        self.d2vy_thu = np.zeros((self.n, self.n, self.nTheta, self.nTheta))
         self.d2logpy_th = np.zeros((self.nTheta, self.nTheta))
-
 
         from rkhs import Green1_eigen
         self.g = Green1_eigen(self.P,self.T,self.thetas[:,0])
 
-        self.u = None
+        from scipy.stats import beta, norm
+        self.u = lambda x:  norm.rvs( 0.3 * beta.pdf(x, 12, 7) + 0.6 * beta.pdf(x, 4, 11), scale = 0.05)
 
         self.y = np.array([7.88231656866e-05, 0.00103141481843, 0.0042599302013, 0.0109414572813, 0.0216727450029, 0.0363139318338,
-            0.0542174735416, 0.0742698619535, 0.0952090587957, 0.115750968435, 0.134715310806, 0.151123127707,
-            0.164276892248, 0.173775284341, 0.179504813305, 0.181458101525, 0.180172458519, 0.175994829843,
-            0.169610986984, 0.161628554675, 0.152658082896, 0.143436721543, 0.134590958132, 0.12662696759,
-            0.119876351997, 0.11459774509, 0.110989979388, 0.108897237248, 0.10826521835, 0.10872970166, 0.10990919735,
-            0.111354787636, 0.11254415309, 0.113050227885, 0.112295064502, 0.110246606493, 0.106490657257,
-            0.101095550495, 0.094295603478, 0.0862019988128, 0.0773258488424, 0.0680937375501, 0.0589552679358,
-            0.050283488449, 0.0424141229229, 0.0355148857564, 0.0296078491161, 0.0246289666774, 0.0204823531847,
-            0.0170388615128])
-
-    def set_u(self,u=None):
-        if u is not None:
-            self.u = u
-        else:
-            from scipy.stats import beta, norm
-            from sympy import symbols
-            s = symbols("s")
-            self.u = norm.rvs(loc = 0.3*beta.pdf(s,12,7) + 0.6*beta.pdf(s,4,11), scale = 0.05)
+                           0.0542174735416, 0.0742698619535, 0.0952090587957, 0.115750968435, 0.134715310806, 0.151123127707,
+                           0.164276892248, 0.173775284341, 0.179504813305, 0.181458101525, 0.180172458519, 0.175994829843,
+                           0.169610986984, 0.161628554675, 0.152658082896, 0.143436721543, 0.134590958132, 0.12662696759,
+                           0.119876351997, 0.11459774509, 0.110989979388, 0.108897237248, 0.10826521835, 0.10872970166, 0.10990919735,
+                           0.111354787636, 0.11254415309, 0.113050227885, 0.112295064502, 0.110246606493, 0.106490657257,
+                           0.101095550495, 0.094295603478, 0.0862019988128, 0.0773258488424, 0.0680937375501, 0.0589552679358,
+                           0.050283488449, 0.0424141229229, 0.0355148857564, 0.0296078491161, 0.0246289666774, 0.0204823531847,
+                           0.0170388615128])
 
     def set_y(self,y):
         self.y = y
 
     # Convolution kernel
     def compute_L_i(self,theta,f,i):
-        from sympy import symbols, integrate
-        from math import exp
+        from scipy import integrate, exp
         i+=1 # indexing in Python for i starts at 0 but we need it to start at 1
-        x = symbols("x")
-        h = exp(-theta[2] * (i*self.tau - x))
-        return integrate(f*h, (x,0,i*self.tau))
+        h = lambda x: exp(-theta[1] * x)
+        return integrate.quad(lambda x: f(x)*h(i*self.tau - x), 0, i*self.tau)[0]
 
     def convolve_eifs(self,theta):
         import numpy as np
@@ -86,10 +71,10 @@ class PB(object):
         return convolved_eifs
 
     # MCMC help
-
     def p_y_given_theta(self,theta):
-        from numpy.random import multivariate_normal
-        p_y_given_th = multivariate_normal(self.y, 0, (self.vy_th + self.vy_th.T) / 2)
+        from scipy.stats import multivariate_normal
+        v = 0.5*(self.vy_th + self.vy_th.T)
+        p_y_given_th = multivariate_normal.pdf(self.y, np.zeros(self.n), v)
         return p_y_given_th
 
     def p_theta(self,theta):
@@ -117,7 +102,6 @@ class PB(object):
 
 
     # Set values of matrices needed for MCMC
-
     def set_d012_lmatrix(self,theta):
         self.lmatrix = self.convolve_eifs(theta)
 
@@ -125,14 +109,24 @@ class PB(object):
         self.dlmatrix[:, :, 1] = - 2 * theta[1] * self.lmatrix
 
         self.d2lmatrix[:, :, 0, 1] = - 2 * theta[1] / theta[0] * self.lmatrix
-        self.d2lmatrix[:, :, 1, 0] = self.d2lmatrix[:, :, 1, 2]
+        self.d2lmatrix[:, :, 1, 0] = self.d2lmatrix[:, :, 0, 1]
         self.d2lmatrix[:, :, 1, 1] = ((4 * theta[1] ** 2) - 2) * self.lmatrix
+
+    def set_vy_thu(self,theta,u):
+        for i in range(self.n):
+            self.vy_thu[i, i] = (0.05 * self.compute_L_i(theta,u,i)) ** 2
+
+    def set_dvy_thu(self,theta,u):
+        pass
+
+    def set_d2vy_thu(self,theta,u):
+        pass
 
     def set_vy_th(self,theta):
         import numpy as np
         for i in range(self.n):
             for k in range(i):
-                self.vy_th[i, k] = sum(np.multiply(np.multiply(self.g.eivs, self.lmatrix[:, i]), self.lmatrix[:, k]))
+                self.vy_th[i, k] = np.sum(np.multiply(np.multiply(self.g.eivs, self.lmatrix[:, i]), self.lmatrix[:, k]))
                 if i != k:
                     self.vy_th[k, i] = self.vy_th[i, k]
         self.vy_th += self.vy_thu
@@ -142,7 +136,7 @@ class PB(object):
         for i in range(self.n):
             for k in range(self.n):
                 for r in range(self.nTheta):
-                    self.dvy_th[i, k, r] = sum(np.multiply(np.multiply(self.g.deivs[:, r],
+                    self.dvy_th[i, k, r] = np.sum(np.multiply(np.multiply(self.g.deivs[:, r],
                                                                        self.lmatrix[:, i]),
                                                            self.lmatrix[:, k]) +
                                                np.multiply(self.g.eivs,(np.multiply(self.dlmatrix[:, i, r],
@@ -150,7 +144,6 @@ class PB(object):
                                                                         np.multiply(self.lmatrix[:, i], self.dlmatrix[:, k, r]))
                                                            )
                                                )
-
         self.dvy_th += self.dvy_thu
 
     def set_d2vy_th(self,theta):
@@ -159,7 +152,7 @@ class PB(object):
             for k in range(self.n):
                 for s in range(self.nTheta):
                     for r in range(self.nTheta):
-                        self.d2vy_th[i, k, s, r] = sum(
+                        self.d2vy_th[i, k, s, r] = np.sum(
                             np.multiply(np.multiply(self.g.d2eivs[:, s, r], self.lmatrix[:, i]), self.lmatrix[:, k]) +
                             np.multiply(self.g.deivs[:, r],
                                         np.multiply(self.dlmatrix[:, i, s], self.lmatrix[:, k]) +
@@ -176,29 +169,20 @@ class PB(object):
 
         self.d2vy_th += self.d2vy_thu
 
-    def set_vy_thu(self,theta,u):
-        for i in range(self.n):
-            self.vy_thu[i, i] = (0.05 * self.compute_L_i(theta,u,i)) ** 2
-
-    def set_dvy_thu(self,theta,u):
-        pass
-
-    def set_d2vy_thu(self,theta,u):
-        pass
-
     def set_d2logpy_th(self,theta):
         import numpy as np
-        assert(self.y.shape[0] == 1)
         s2 = self.d2vy_th
         s1 = self.dvy_th
         s0 = self.vy_th
         for s in range(self.nTheta):
             for r in range(self.nTheta):
                 self.d2logpy_th[s, r] = - 0.5 * (np.trace(np.linalg.solve(s0, (s2[:, :, s, r]))) -
-                                            np.trace(np.dot(np.linalg.solve(s0, (s1[:, :, s] / s0)), s1[:, :, r])) +
-                                            np.dot(np.dot(self.y,
-                                                          np.linalg.solve(s0, (np.dot(s1[:, :, s] / s0, s1[:, :, r]) -
-                                                                              s2[:, :, s, r] + np.dot(s1[:, :, r] / s0, s1[:, :, s]))) / s0), self.y.T))
+                                                 np.trace(np.dot(np.linalg.solve(s0, (s1[:, :, s] / s0)), s1[:, :, r])) +
+                                                 np.dot(np.dot(self.y,
+                                                               np.linalg.solve(s0, (np.dot(s1[:, :, s] / s0, s1[:, :, r]) -
+                                                                                    s2[:, :, s, r] + np.dot(s1[:, :, r] / s0, s1[:, :, s]))) / s0),
+                                                        self.y.T)
+                                                 )
 
     def set_vhat(self,theta = [1,10]):
         from numpy.linalg import inv
@@ -208,16 +192,17 @@ class PB(object):
 
     def calculate_new_operators(self,theta):
         self.g.set_theta(theta)
-        self.set_vy_thu(theta)
-        self.set_dvy_thu(theta)
-        self.set_d2vy_thu(theta)
+        self.set_vy_thu(theta,self.u)
+        self.set_dvy_thu(theta,self.u)
+        self.set_d2vy_thu(theta,self.u)
         self.set_d012_lmatrix(theta)
         self.set_vy_th(theta)
         self.set_dvy_th(theta)
         self.set_d2vy_th(theta)
         self.set_d2logpy_th(theta)
+        # self.set_vhat(theta)
 
-
+"""
     def plot_mcmc_results(self):
         fks = cell(1, K - burnin)
 
@@ -239,25 +224,28 @@ class PB(object):
         plot(fU[t], 'r--')
         # Scatterplot thetas figure
         scatter(thetas[1, burnin:end()], thetas[2, burnin:end()], '.')
-
+"""
 
 
 if __name__ == "__main__":
 
-    from scipy.stats import norm,truncnorm,multivariate_normal
+    from scipy.stats import norm, truncnorm, multivariate_normal
     import numpy as np
 
     pb = PB()
 
+    pb.calculate_new_operators(pb.thetas[:, 0])
+    pb.set_vhat(pb.thetas[:, 0])
+
     rejected = 0
-    ## Loop
+
     while pb.k < pb.K:
 
         if (pb.k % 500) != 0:
             print pb.k
 
         # Restricted to be nonnegative
-        pb.thetas[:, pb.k] = pb.thetas[:, pb.k - 1] + np.sqrt(pb.alph) * np.linalg.cholesky(pb.vhat) * truncnorm.rvs(0,np.Infinity, size = pb.nTheta) # pillonetto_bell/src/mcmc.m:88
+        pb.thetas[:, pb.k] = pb.thetas[:, pb.k - 1] + np.sqrt(pb.alph) * np.dot(np.linalg.cholesky(pb.vhat), truncnorm.rvs(0,np.Infinity, size = pb.nTheta))
         pb.calculate_new_operators( pb.thetas[:, pb.k] )
 
         try:
--- pb_py/done/rkhs.py
+++ pb_py/done/rkhs.py
@@ -17,21 +17,20 @@ class Green1_eigen(object):
 
         import numpy as np
         self.eivs = np.zeros(P)
-        self.deivs = np.zeros(P)
-        self.d2eivs = np.zeros(P)
+        self.deivs = np.zeros((P, theta.shape[0]))
+        self.d2eivs = np.zeros((P, theta.shape[0], theta.shape[0]))
 
         from math import pi, sqrt, sin
-        from sympy import symbols, lambdify
 
-        s, theta = symbols("s,theta")
-        for j in range(1, self.P + 1):
-            self.eivs[j] = self.theta[0] * (self.T ** 2) / (((j - 1) * pi) + pi / 2) ** 2
-            self.eifs[j] = lambdify(s, sqrt(2 / self.T) * sin((s / self.T) * (j * pi - pi / 2)), "numpy")
-            self.deivs[j] = (self.T ** 2) / (((j - 1) * pi) + pi / 2) ** 2
+        for j in range(self.P):
+            self.eivs[j] = self.theta[0] * (self.T ** 2) / ((j * pi) + pi / 2) ** 2
+            self.eifs[j] = lambda s: sqrt(2 / self.T) * sin((s / self.T) * (j * pi - pi / 2))
+            self.deivs[j] = (self.T ** 2) / ((j * pi) + pi / 2) ** 2
             self.d2eivs[j] = 0
 
     def set_theta(self,theta):
-        for j in range(1,self.P + 1):
-            self.eivs[j] = theta[0] * (self.T ** 2) / (((j - 1) * pi) + pi / 2) ** 2
-            self.deivs[j] = (self.T ** 2) / (((j - 1) * pi) + pi / 2) ** 2
+        from math import pi
+        for j in range(self.P):
+            self.eivs[j] = theta[0] * (self.T ** 2) / ((j * pi) + pi / 2) ** 2
+            self.deivs[j] = (self.T ** 2) / ((j * pi) + pi / 2) ** 2
             self.d2eivs[j] = 0
